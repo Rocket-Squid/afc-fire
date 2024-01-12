be1ac7b19efdfeeb2597b9315547546d22057bbb
Matej navara <matej.navara@gmail.com>
2024-01-13 00:00:59 +0800

    Fix Favicon

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,36 @@
-import { Head, Html, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
-    return (
-        <Html lang="en" id="#top">
-            <Head />
-            <link rel="shortcut icon" href="/assets/img/favicon.png" />
-            <link rel="preconnect" href="https://fonts.bunny.net" />
-            <link href="https://fonts.bunny.net/css?family=dm-sans:400,400i,500,500i,700,700i|poppins:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i" rel="stylesheet" />
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    )
+  return (
+    <Html lang="en" id="#top">
+      <Head />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/assets/favicon/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/assets/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/assets/favicon/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/assets/favicon/site.webmanifest" />
+      <link rel="preconnect" href="https://fonts.bunny.net" />
+      <link
+        href="https://fonts.bunny.net/css?family=dm-sans:400,400i,500,500i,700,700i|poppins:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i"
+        rel="stylesheet"
+      />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }

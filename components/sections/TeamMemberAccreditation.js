980c288df9a3dcd0b2a294cd9245beb6b5315cbf
Matej navara <matej.navara@gmail.com>
2024-04-23 11:29:36 +0800

    More accreditation fixes

--- components/sections/TeamMemberAccreditation.js
+++ components/sections/TeamMemberAccreditation.js
@@ -1,5 +1,6 @@
 import Slider from "react-slick";
 import { ACCRED_LIST } from "@/lib/accreditation";
+import Link from "next/link";
 
 const brandSlider = {
   dots: false,
@@ -42,7 +43,11 @@ const brandSlider = {
     },
   ],
 };
-export default function TeamMemberAccreditation({ accreditation }) {
+export default function TeamMemberAccreditation({
+  accreditation,
+  fracsUrl,
+  ifsmUrl,
+}) {
   const generateAccreditationArray = (accreditation) => {
     const generatedArray = [];
     if (!accreditation || accreditation.length === 0) return generatedArray;
@@ -51,36 +56,42 @@ export default function TeamMemberAccreditation({ accreditation }) {
       generatedArray.push({
         name: "Warrington Fire - FRACS Individual Scheme",
         filepath: "/assets/img/accreditation/FRACS.png",
+        link: fracsUrl || null,
       });
     }
     if (accreditation.includes(ACCRED_LIST.MEMBER)) {
       generatedArray.push({
         name: "IFSM Member",
         filepath: "/assets/img/accreditation/IFSM-Logo-Member.png",
+        link: ifsmUrl || null,
       });
     }
     if (accreditation.includes(ACCRED_LIST.ASSOCIATE)) {
       generatedArray.push({
         name: "IFSM Associate",
         filepath: "/assets/img/accreditation/IFSM-Logo-Associate.png",
+        link: ifsmUrl || null,
       });
     }
     if (accreditation.includes(ACCRED_LIST.TIER1)) {
       generatedArray.push({
         name: "IFSM Tier 1 - 2024",
         filepath: "/assets/img/accreditation/Tier-1-Logo.png",
+        link: ifsmUrl || null,
       });
     }
     if (accreditation.includes(ACCRED_LIST.TIER2)) {
       generatedArray.push({
         name: "IFSM Tier 2 - 2024",
         filepath: "/assets/img/accreditation/Tier-2-Logo.png",
+        link: ifsmUrl || null,
       });
     }
     if (accreditation.includes(ACCRED_LIST.TIER3)) {
       generatedArray.push({
         name: "IFSM Tier 3 - 2023",
         filepath: "/assets/img/accreditation/Tier-3-Logo.png",
+        link: ifsmUrl || null,
       });
     }
 
@@ -95,7 +106,13 @@ export default function TeamMemberAccreditation({ accreditation }) {
             {generateAccreditationArray(accreditation).map((cred) => (
               <div className="col-12" key={cred.name}>
                 <div className="brand-item">
-                  <img src={cred.filepath} alt={cred.name} />
+                  {cred.link ? (
+                    <Link href={cred.link}>
+                      <img src={cred.filepath} alt={cred.name} />
+                    </Link>
+                  ) : (
+                    <img src={cred.filepath} alt={cred.name} />
+                  )}
                 </div>
               </div>
             ))}

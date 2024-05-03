d1d127cde91601cd46beecaf8429b4d89cb69ea9
Matej navara <matej.navara@gmail.com>
2024-05-03 15:15:45 +0800

    Fixed broken import

--- pages/services.js
+++ pages/services.js
@@ -1,27 +1,7 @@
 import Layout from "@/components/layout/Layout";
-import Link from "next/link";
-import { useState } from "react";
-import Counter2 from "@/components/sections/Counter2";
 import Services from "@/components/sections/Services";
 
 export default function Service() {
-  const [isActive, setIsActive] = useState({
-    status: false,
-    key: "",
-  });
-
-  const handleToggle = (key) => {
-    if (isActive.key === key) {
-      setIsActive({
-        status: false,
-      });
-    } else {
-      setIsActive({
-        status: true,
-        key,
-      });
-    }
-  };
   return (
     <>
       <Layout breadcrumbTitle="Services">

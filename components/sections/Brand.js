7b7e690d93ffed9b40b963c461592268f4c08b67
Matej Navara <matej.navara@gmail.com>
2026-01-20 17:41:11 +0800

    Fix brand logo sizing and centering

--- components/sections/Brand.js
+++ components/sections/Brand.js
@@ -69,7 +69,13 @@ export default function Brand() {
                     href="https://www.nsi.org.uk/company-finder/?at=&type=quick&familyType=&town-postcode=&Lo=&La=&sortby=distance&range=2&search=company&company-name=AFC+Fire"
                     target="_blank"
                     rel="noopener noreferrer"
-                    style={{ cursor: "pointer" }}
+                    style={{
+                      cursor: "pointer",
+                      height: "100%",
+                      width: "100%",
+                      alignContent: "center",
+                      justifyItems: "center",
+                    }}
                   >
                     <img
                       src="/assets/img/accreditation/NSI-SILVER-UKAS-transparent.png"
